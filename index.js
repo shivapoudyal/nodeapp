10332718525053e3b5fe4f3a0411dc98fff5df20
root <root@ip-172-31-59-128.ec2.internal>
2020-07-26 05:22:59 +0000

    committed by shiva

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const db = "mongodb://172.17.0.4:27017/dbname";
+
+mongoose
+ .connect(db,{
+     useNewUrlParser: true,
+     useCreateIndex: true,
+     useUnifiedTopology: true
+ })
+ .then(() => {
+     console.log("DB Connected");
+ })
+ .catch(() => {
+    console.log("Error in db connection");
+})
+ 
+
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send('Hello World From NodeJs');
+    res.send('Node js running on node container');
 })
 
 app.listen(8000, () => {
